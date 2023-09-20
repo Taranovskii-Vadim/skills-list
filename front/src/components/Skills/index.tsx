05bdf717c074af8e756287f4e776e31e518f4a82
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-09-20 20:26:02 +0400

    render skill

--- front/src/components/Skills/index.tsx
+++ front/src/components/Skills/index.tsx
@@ -1,5 +1,12 @@
 import { useEffect } from 'react';
+import { Rating } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
+import List from '@mui/material/List';
+import Avatar from '@mui/material/Avatar';
+import ListItem from '@mui/material/ListItem';
+import ImageIcon from '@mui/icons-material/Image';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
 
 import { RootState } from '../../store/types';
 import { fetchSkills } from '../../store/skills/actions';
@@ -21,7 +28,21 @@ const Skills = ({ categoryId }: Props) => {
     return <div>loading...</div>;
   }
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <List sx={{ width: '100%' }}>
+      {data.map((item) => (
+        <ListItem key={item.id}>
+          <ListItemAvatar>
+            <Avatar>
+              <ImageIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={item.name} secondary="Jan 9, 2014" />
+          <Rating max={10} value={item.rate} />
+        </ListItem>
+      ))}
+    </List>
+  );
 };
 
 export default Skills;
--- front/src/style/index.ts
+++ front/src/style/index.ts
@@ -4,6 +4,16 @@ const getTheme = (mode: PaletteMode) => {
   return createTheme({
     palette: { mode },
     components: {
+      MuiListItem: {
+        styleOverrides: {
+          root: {
+            borderRadius: '8px',
+            marginBottom: '16px',
+            ':last-child': { marginBottom: 0 },
+            boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
+          },
+        },
+      },
       MuiRating: { defaultProps: { precision: 0.5 } },
       MuiAppBar: { styleOverrides: { root: { padding: 0 } } },
       MuiPaper: { styleOverrides: { root: { padding: '16px 24px' } } },

1971ad7b8e4606b5e71198f00053286b6a1a3a09
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-09-28 16:57:38 +0400

    refactor

--- front/src/pages/Main/index.tsx
+++ front/src/pages/Main/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Grid, Paper } from '@mui/material';
+import { Grid } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { Dispatch, RootState } from 'src/store/types';
@@ -21,16 +21,12 @@ const Main = () => {
     <>
       <Header />
       <Grid container spacing={3} sx={{ m: 0, width: '100%', pr: 3 }}>
-        <Grid item xs={8} sx={{ pt: 0 }}>
+        <Grid item xs={12} sx={{ pt: 0 }}>
           {!isLoading ? (
             <Tabs items={data.map(({ id, title }) => ({ label: title, node: <Skills categoryId={id} /> }))} />
           ) : (
             <div>loading...</div>
           )}
-        </Grid>
-        <Grid item xs={4} sx={{ pt: 0 }}>
-          <Paper>block for skills charts 1</Paper>
-          <Paper sx={{ mt: 3 }}>block for skills charts 2</Paper>
         </Grid>
       </Grid>
     </>

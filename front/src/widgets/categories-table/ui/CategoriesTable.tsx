f76578145e20a0dddf6f65f5d034747e5ffb6df7
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-12-24 21:12:55 +0400

    refactor fsd

---
+++ front/src/widgets/categories-table/ui/CategoriesTable.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+import { TableRow, TableCell } from '@mui/material';
+
+import Table from '@shared/ui/mui/Table';
+
+import useCategories from '../model/store';
+
+const COLUMNS = ['Наименование', 'Количество навыков', 'Автор', 'Дата создания', 'Последнее изменение'];
+
+const CategoriesTable = () => {
+  const { data, loading, error, fetchData } = useCategories();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  if (error) {
+    return <div>error...</div>;
+  }
+
+  if (!data.length) {
+    return <div>nothing to show...</div>;
+  }
+
+  return (
+    <Table columns={COLUMNS}>
+      {data.map((row) => (
+        <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+          <TableCell>{row.name}</TableCell>
+          <TableCell>{row.numberOfSkills}</TableCell>
+          <TableCell>{row.author}</TableCell>
+          <TableCell>{row.createdAt}</TableCell>
+          <TableCell>{row.updatedAt}</TableCell>
+        </TableRow>
+      ))}
+    </Table>
+  );
+};
+
+export default CategoriesTable;
---
+++ front/src/widgets/page-header/ui/PageHeader.tsx
@@ -0,0 +1,18 @@
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+type Props = { title: string; onClick: Maybe<() => void> };
+
+const PageHeader = ({ title, onClick }: Props) => (
+  <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ mb: 3 }}>
+    <Typography variant="h4">{title}</Typography>
+    {onClick ? (
+      <Button variant="contained" onClick={onClick}>
+        Добавить
+      </Button>
+    ) : null}
+  </Stack>
+);
+
+export default PageHeader;
